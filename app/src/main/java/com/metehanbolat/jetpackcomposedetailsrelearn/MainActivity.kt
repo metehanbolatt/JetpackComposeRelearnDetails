7070fa05fc97a42f4b1e2fd29da0aa156284e133
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-12-01 01:14:23 +0300

    Column - Vertical Arrangement Features

--- app/src/main/java/com/metehanbolat/jetpackcomposedetailsrelearn/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/jetpackcomposedetailsrelearn/MainActivity.kt
@@ -3,18 +3,14 @@ package com.metehanbolat.jetpackcomposedetailsrelearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.metehanbolat.jetpackcomposedetailsrelearn.ui.theme.JetpackComposeDetailsRelearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,30 +30,30 @@ fun MainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ){
-        Row(
+        Column(
             modifier = Modifier.fillMaxSize(),
             /** SpaceAround Composable'lar arasına koyduğu boşluğun yarısı kadar start/end margin verir.*/
             /** SpaceEvenly Composable'lar arasına koyduğu boşluk kadar start-end margin verir..*/
             /** SpaceBetween Start/End parent olarak uygulayıp aralardaki composable'lara eşit boşluk verir. */
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            HorizontalColoredBar(Color.Magenta)
-            HorizontalColoredBar(Color.Red)
-            HorizontalColoredBar(Color.Green)
-            HorizontalColoredBar(Color.Cyan)
-            HorizontalColoredBar(Color.White)
+            VerticalColoredBar(Color.Magenta)
+            VerticalColoredBar(Color.Red)
+            VerticalColoredBar(Color.Green)
+            VerticalColoredBar(Color.Cyan)
+            VerticalColoredBar(Color.White)
         }
     }
 }
 
 @Composable
-fun HorizontalColoredBar(color: Color) {
+fun VerticalColoredBar(color: Color) {
     Surface(
         color = color,
         modifier = Modifier
-            .height(600.dp)
-            .width(60.dp)
+            .height(60.dp)
+            .fillMaxWidth()
     ){ }
 }
 
