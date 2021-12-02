e2daf763a2bb9b5a483c33599d651fa0be9d21d9
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-12-02 14:04:34 +0300

    Profile picture and name in Card

--- app/src/main/java/com/metehanbolat/jetpackcomposedetailsrelearn/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/jetpackcomposedetailsrelearn/MainActivity.kt
@@ -3,15 +3,20 @@ package com.metehanbolat.jetpackcomposedetailsrelearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -37,11 +42,15 @@ fun MainScreen() {
 @Composable
 fun ProfileCard() {
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
         elevation = 8.dp
     ) {
         Row(
-            modifier = Modifier.wrapContentSize()
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             ProfilePicture()
             ProfileContent()
@@ -51,12 +60,27 @@ fun ProfileCard() {
 
 @Composable
 fun ProfilePicture() {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.Green),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Profile Image"
+        )
+    }
 
 }
 
 @Composable
 fun ProfileContent() {
-
+    Text(
+        text = "Metehan Bolat"
+    )
 }
 
 @Preview(showBackground = true)
